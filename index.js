39f57b09181816276a7e61675ef6dca7216ed07e
pedrobando <pedrobandorivera@gmail.com>
2021-10-06 18:34:13 -0500

    Create a new variable and Call the newly created function with the string no as a paramete as a value to the variabler. Console log the results.

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ function randomItemFromArray(arrayOne){
     return arrayOne[Math.floor(Math.random() * arrayOne.length)];
 }
 function calculateTrip(response){
+    let des;
+    let res;
+    let tra;
+    let ent;
     response = "no";
     while(happy == false){
-    let des = randomItemFromArray(["Milwaukee", "Orlando", "Denver", "Chicago"]);
-    let res = randomItemFromArray(["Red Lobster", "Ruby Tuesday", "Fridays", "Red Robins"]);
-    let tra = randomItemFromArray(["walking", "a flight", "uber", "lyft"]);
-    let ent = randomItemFromArray(["Museum", "Aquarium", "Baseball Stadium", "Water park"]);
-    alert("Your Day Trip Looks like the destination is " + des + " the restaurant is " + res + " the mode of transport is " + tra + " and the entertainment is " + ent);
-    let response = prompt("Are you satisfied? Please type YES or NO");
-    if(response == "yes"){
-        happy = true; 
-        console.log(des + " "+" "+ res +" "+ tra+" " + ent); 
+        des = randomItemFromArray(["Milwaukee", "Orlando", "Denver", "Chicago"]);
+        res = randomItemFromArray(["Red Lobster", "Ruby Tuesday", "Fridays", "Red Robins"]);
+        tra = randomItemFromArray(["walking", "a flight", "uber", "lyft"]);
+        ent = randomItemFromArray(["Museum", "Aquarium", "Baseball Stadium", "Water park"]);
+        alert("Your Day Trip Looks like the destination is " + des + " the restaurant is " + res + " the mode of transport is " + tra + " and the entertainment is " + ent);
+        let response = prompt("Are you satisfied? Please type YES or NO");
+        if(response == "yes"){
+            happy = true; 
+            console.log(des + " "+" "+ res +" "+ tra+" " + ent); 
+        }
+        else{ 
+            happy = false
+        }
     }
-    else{ 
-    happy = false 
-    }
-}
+
     return [des, res, tra, ent]
 }
 
-calculateTrip("no")
+let trip = calculateTrip("no");
+console.log(`Congratulations! Your Destinations is ${trip[0]}, the restaurant is ${trip[1]}, you are getting there by ${trip[2]} and you will be entertain with ${trip[3]}`);
